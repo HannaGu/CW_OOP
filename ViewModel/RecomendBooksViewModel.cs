f46a57288e49fd6a54454118af38321d53a784bd
HannaGu <70803052+HannaGu@users.noreply.github.com>
2021-05-12 23:35:40 +0300

    Add files via upload

---
+++ ViewModel/RecomendBooksViewModel.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CW_WPF.Model;
+using CW_WPF.View;
+using CW_WPF.DB;
+using DevExpress.Mvvm;
+using System.Windows.Input;
+using System.Windows.Controls;
+using System.Collections.ObjectModel;
+
+namespace CW_WPF.ViewModel
+{
+    public class RecomendBooksViewModel : ViewModelBase
+    {
+        public ObservableCollection<Book> All_Books { get; set; }
+        DB_GetItems db_GetItems = new DB_GetItems();
+       RecomendViewModel Obj;
+        public RecomendBooksViewModel(RecomendViewModel obj)
+        {
+            All_Books = GetRecomendBooks();
+            Obj = obj;
+        }
+        private int index;
+        public int Index
+        {
+            get
+            {
+                return index;
+            }
+            set
+            {
+                this.index = value;
+                RaisePropertiesChanged(nameof(Index));
+            }
+        }
+
+        public ICommand open_BookPage => new DelegateCommand(OpenBookPage);
+
+        private void OpenBookPage()
+        {
+            Page BookInfo = new RecomendBookPage(All_Books[Index], Obj);
+            Obj.CurrentPage = BookInfo;
+        }
+
+        public ObservableCollection<Book> GetRecomendBooks()
+        {
+            ObservableCollection<Book> books = new ObservableCollection<Book>();
+            ObservableCollection<Book> userBooks = new ObservableCollection<Book>();
+            ObservableCollection<Book> result = new ObservableCollection<Book>();
+            DB_GetItems db_GetItems = new DB_GetItems();
+            books = db_GetItems.GetBook();
+            userBooks = db_GetItems.GetUserBook();
+            foreach (Book ub in userBooks)
+            {
+                var selectedBooks = from b in books
+                                    where b.Ganre.Equals(ub.Ganre) || b.Author.ToUpper().Equals(ub.Author.ToUpper())
+                                    select b;
+                foreach (Book s in selectedBooks)
+                {
+                    result.Add(s);
+                }
+            }
+            return result;
+        }
+
+    }
+}
+
--- ViewModel/RecomendViewModel.cs
+++ ViewModel/RecomendViewModel.cs
@@ -3,10 +3,41 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DevExpress.Mvvm;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+using CW_WPF.Model;
+using CW_WPF.View;
+using CW_WPF.ViewModel;
+using CW_WPF.DB;
+
 
 namespace CW_WPF.ViewModel
 {
-    class RecomendViewModel
+    public class RecomendViewModel:ViewModelBase
     {
+        private Page All_BooksPage;
+
+        public RecomendViewModel()
+        {
+            All_BooksPage = new RecomendBooks(this);
+            CurrentPage = All_BooksPage;
+        }
+
+        private Page currentpage;
+        public Page CurrentPage
+        {
+            get
+            {
+                return currentpage;
+            }
+            set
+            {
+                this.currentpage = value;
+                RaisePropertiesChanged(nameof(CurrentPage));
+            }
+        }
     }
 }
+
