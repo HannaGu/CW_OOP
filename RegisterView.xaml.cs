1508807fdaf31ca0b65109d97f731c202a56fcae
HannaGu <70803052+HannaGu@users.noreply.github.com>
2021-05-28 06:54:58 +0300

    Add files via upload

---
+++ RegisterView.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using CW_WPF.ViewModel;
+
+
+namespace CW_WPF.View
+{
+    public partial class RegisterView : Window
+    {
+        RegisterViewModel reg = new RegisterViewModel();
+        public RegisterView()
+        {
+            InitializeComponent();
+            DataContext = reg;
+            WindowStartupLocation = WindowStartupLocation.CenterScreen;
+        }
+
+        private void PasswordBox_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            reg.password = password_textbox.Password;
+        }
+
+        private void TextBox_PreviewTextInput_1(object sender, TextCompositionEventArgs e)
+        {
+            Regex regex = new Regex("[^0-9]+");
+            e.Handled = regex.IsMatch(e.Text);
+        }
+        private void TextBox_PreviewTextInput_2(object sender, TextCompositionEventArgs e)
+        {
+            Regex regex = new Regex("^[0-9]+|^[,]+");
+            e.Handled = !regex.IsMatch(e.Text);
+        }
+
+
+    }
+}
---
+++ UserBookPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using DevExpress.Mvvm;
+
+
+using CW_WPF.Model;
+using CW_WPF.ViewModel;
+
+
+namespace CW_WPF.View
+{
+    public partial class UserBookPage : Page
+    {
+        UserBookViewModel ubvm;
+        public UserBookPage(Book obj1, UserLibraryViewModel obj2)
+        {
+            ubvm = new UserBookViewModel(obj1, obj2);
+            InitializeComponent();
+            DataContext = ubvm;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
